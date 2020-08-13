6b73b52f5c50ae744f19131f0830e112984b404b
Jimmique <jimmique88@gmail.com>
2020-08-13 02:58:38 -0700

    Added swp to gitignore and completed single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # Sort the array
+    arr.sort()
 
-    pass
+    # Set up index tracker
+    index = 0
+
+    # Iterate through the integers, incrementing by 2 each time
+    while(index < len(arr)):
+        # If the integer at the current index is not equal to the next integer
+        # That's the solution
+        if arr[index] != arr[index + 1]:
+            return arr[index]
+        index += 2
+
+    return None
 
 
 if __name__ == '__main__':
