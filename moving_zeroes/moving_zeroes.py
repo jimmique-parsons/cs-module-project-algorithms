b2b96545e0cd1a1c689f69b6ada13cc54256bf9d
Jimmique <jimmique88@gmail.com>
2020-08-13 03:17:51 -0700

    Completed moving_zeroes.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,12 +4,20 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # Tracker for last non zero int 
+    lastNonZeroIdx = 0
 
-    pass
+    for i in range(0, len(arr)):
+        if arr[i] != 0:
+            arr[i], arr[lastNonZeroIdx] = arr[lastNonZeroIdx], arr[i]
+            lastNonZeroIdx += 1
+        print(arr)
 
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
+    moving_zeroes(arr)
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
